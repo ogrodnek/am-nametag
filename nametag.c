c3cc1570d618e1ca9a985ae6f9070429df6459b7
Larry Ogrodnek <ogrodnek@gmail.com>
2011-07-24 17:21:03 -0700

    multiple modes!  allOn and blink!

--- nametag.c
+++ nametag.c
@@ -1,10 +1,22 @@
 #include <avr/io.h>
+#include <avr/eeprom.h> 
 
 void setup(void);
 void loop(void);
+void delay_ms(uint16_t ms);
+
+uint8_t EEMEM ProgramConfig = 0;
+int program = 0;
 
 int main(void) {
   setup();
+
+  program = eeprom_read_byte(&ProgramConfig);
+  if (program > 1) {
+    program = 0;
+  }
+
+  eeprom_write_byte(&ProgramConfig, 1 - program);
   while (1) {
     loop();
   }
@@ -13,8 +25,8 @@ int main(void) {
 }
 
 void setup() {
-  PORTB = 0; // all off
   DDRB = 255; // all output
+  PORTB = 0;
 }
 
 const uint8_t states[] = {
@@ -26,6 +38,33 @@ const uint8_t states[] = {
 };
 
 void loop() {
+  if (program) {
+    blink();
+  } else {
+    allOn();
+  }
+}
+
+int blinkState = 0;
+unsigned long lastBlink = 0;
+unsigned long clock = 0;
+
+void blink() {
+
+  if (clock - lastBlink > 15000) {
+    blinkState++;
+    if (blinkState > 4) {
+      blinkState = 0;
+    }
+    lastBlink = clock;
+  }
+
+  PORTB = states[blinkState];
+
+  clock++;
+}
+
+void allOn() {
   int i;
   for (i=0; i< 5; i++) {
     PORTB = states[i];
